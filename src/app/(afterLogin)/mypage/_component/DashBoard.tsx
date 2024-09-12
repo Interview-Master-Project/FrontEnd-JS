6e6231506c1fcd67f0bef8bd86079b972ffd99cf
tim1007 <111577841+yuzunsang@users.noreply.github.com>
2024-09-12 17:44:39 +0900

    Feat: 조건부 스타일링 clsx로 변경 및 Dashboard.tsx 리팩토링

--- src/app/(afterLogin)/mypage/_component/DashBoard.tsx
+++ src/app/(afterLogin)/mypage/_component/DashBoard.tsx
@@ -1,32 +1,11 @@
 import styles from "./dashBoard.module.scss";
-import cx from "classnames";
-import { generateLogData } from "@/app/(afterLogin)/mypage/_function/generateLogData";
-import dayjs from "dayjs";
+import { checkBoard, calculateData } from "../_lib/calculateData";
+// import cx from "classnames";
+import clsx from "clsx";
 
 export default function DashBoard() {
-  const CHECK_ROW = 7;
-  const CHECK_COL = 20;
-
-  // 0. 더미 데이터 로그 생성
-  // 인수 - 현재 날짜
-  const data = generateLogData(dayjs().format("YYYY-MM-DD"));
-
-  // 1. 7열 20행짜리 배열 생성
-  const checkBoard = Array.from({ length: CHECK_ROW }, (_, rowIndex) =>
-    Array.from({ length: CHECK_COL }, (_, colIndex) => {
-      return { rowIndex, colIndex };
-    })
-  );
-
-  // 2. count 최대 값 계산
-  const countsArr: number[] = [];
-  data.map(({ count }) => countsArr.push(count));
-  const maxCount = Math.max(...countsArr);
-
-  // 3. 과거 데이터 누락 시, 즉 데이터가 충분치 않은 경우 예외 테스트
-  for (let i = 0; i < 20; i++) {
-    data.pop();
-  }
+  const data = calculateData(); // dummy data 생성
+  const maxCount = Math.max(...data.map((log) => log.count), 0);
 
   return (
     <div className={styles.dashboard}>
@@ -35,27 +14,35 @@ export default function DashBoard() {
         <tbody>
           {checkBoard.map((innerArr, rowIndex) => (
             <tr key={`${rowIndex}_${Math.random()}`}>
-              {innerArr.map((box, colIndex) => {
+              {innerArr.map((_, colIndex) => {
                 // data 배열에서 해당하는 rowIndex와 colIndex를 가진 데이터가 있는 지 확인
-                let colorGrade = cx();
                 const targetData = data.find(
                   (log) =>
                     log.colIndex === colIndex && log.rowIndex === rowIndex
                 );
-                if (targetData === undefined || targetData.count === 0) {
-                  colorGrade = cx(styles.boardNone);
-                } else if (maxCount / 3 > targetData.count) {
-                  colorGrade = cx(styles.boardLight);
-                } else if (maxCount / 1.5 > targetData.count) {
-                  colorGrade = cx(styles.boardBase);
-                } else {
-                  colorGrade = cx(styles.boardDark);
-                }
 
                 return (
                   <td
                     key={`${colIndex}_${Math.random()}`}
-                    className={colorGrade}
+                    className={clsx({
+                      [styles.boardNone]:
+                        targetData === undefined || targetData.count === 0,
+                      [styles.boardLight]:
+                        targetData &&
+                        targetData.count > 0 &&
+                        targetData.count <= maxCount / 3,
+                      [styles.boardBase]:
+                        targetData &&
+                        targetData.count > maxCount / 3 &&
+                        targetData.count <= maxCount / 1.5,
+                      [styles.boardDark]:
+                        targetData && targetData.count > maxCount / 1.5,
+                    })}
+                    title={
+                      targetData
+                        ? `날짜: ${targetData.date} 제출 수: ${targetData.count}`
+                        : "제출 이력이 없습니다."
+                    }
                   ></td>
                 );
               })}
---
+++ src/app/(afterLogin)/mypage/_lib/calculateData.ts
@@ -0,0 +1,25 @@
+import { generateLogData } from "./generateLogData";
+import dayjs from "dayjs";
+
+const CHECK_ROW = 7;
+const CHECK_COL = 20;
+
+// 7열 20행짜리 배열 생성
+export const checkBoard = Array.from({ length: CHECK_ROW }, (_, rowIndex) =>
+  Array.from({ length: CHECK_COL }, (_, colIndex) => {
+    return { rowIndex, colIndex };
+  })
+);
+
+export function calculateData() {
+  // 더미 데이터 로그 생성
+  // 인수 - 현재 날짜
+  const data = generateLogData(dayjs().format("YYYY-MM-DD"));
+
+  // 과거 데이터 누락 시, 즉 데이터가 충분치 않은 경우 예외 테스트
+  for (let i = 0; i < 20; i++) {
+    data.pop();
+  }
+
+  return data;
+}
