d4b8fc89e6f6de698199c992f74cab2828ac7a4c
tim1007 <111577841+yuzunsang@users.noreply.github.com>
2024-09-17 16:22:57 +0900

    Feat: 상세 컬렉션 페이지 Data Fetching

--- src/app/(afterLogin)/_component/Container.tsx
+++ src/app/(afterLogin)/_component/Container.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from "react";
 import styles from "./Container.module.scss";
 
-type Props = { title: string; children: ReactNode };
+type Props = { title?: string; children: ReactNode };
 
-export default function Container({ title, children }: Props) {
+export default function Container({ title = "", children }: Props) {
   return (
     <div className={styles.container}>
       <h3>{title}</h3>
--- src/app/(afterLogin)/mypage/collections/page.tsx
+++ src/app/(afterLogin)/mypage/collections/page.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import logo from "../../../../../public/logo.png";
 import { BsThreeDots } from "react-icons/bs";
 import { useState } from "react";
+import type { StaticImageData } from "next/image";
 
 export default function Page() {
   const [collDropdown, setCollDropdown] = useState(false);
@@ -16,32 +17,81 @@ export default function Page() {
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
 
-  console.log(`id : ${id}인 컬렉션 페이지`);
-
   const handleCollDropdown = () => {
     setCollDropdown((prevState) => !prevState);
   };
 
+  type COLL = {
+    imgUrl: string | StaticImageData;
+    access: "PRIVATE" | "PUBLIC";
+    title: string;
+    quiz: QUIZ[];
+  };
+
+  type QUIZ = {
+    id: number;
+    title: string;
+    rate?: number;
+    createdAt: string;
+    solvedAt?: string;
+    category: CATEGORY;
+  };
+
+  type CATEGORY = {
+    category: string;
+    collection: string;
+  };
+
+  // dummy data
+  const COLL_DATA: COLL = {
+    imgUrl: logo,
+    access: "PRIVATE",
+    title: "Java 면접 질문 리스트",
+    quiz: [
+      {
+        id: 1077,
+        title: "String은 왜 불변 객체인가?",
+        rate: 40,
+        createdAt: "2024-09-16",
+        solvedAt: "2024-09-16",
+        category: { category: "IT/소프트웨어", collection: "Java" },
+      },
+      {
+        id: 1076,
+        title: "당신의 가장 소중한 것은 무엇입니까?",
+        rate: 100,
+        createdAt: "2024-09-10",
+        solvedAt: "2024-09-17",
+        category: { category: "인성", collection: "일반" },
+      },
+    ],
+  };
+
   return (
     <>
-      <Container title="">
+      <Container>
         <div className={styles.infoWrapper}>
           <BsThreeDots
             className={styles.infoEditBtn}
             onClick={handleCollDropdown}
           />
           {collDropdown && (
-            <div className={styles.profileEditDropdown}>
+            <div className={styles.collEditDropdown}>
               <div className={styles.dropdownItem}>수정</div>
               <div className={styles.dropdownItem}>삭제</div>
             </div>
           )}
-          <Image src={logo} alt="임시 이미지" width={80} height={80} />
+          <Image
+            src={COLL_DATA.imgUrl}
+            alt="임시 이미지"
+            width={80}
+            height={80}
+          />
           <div>
             <div className={styles.collectionAccess}>
-              <span>Public</span>
+              <div>{COLL_DATA.access === "PRIVATE" ? "Private" : "Public"}</div>
             </div>
-            <h4>Java 면접 질문 리스트</h4>
+            <h4>{COLL_DATA.title}</h4>
           </div>
         </div>
       </Container>
@@ -50,6 +100,20 @@ export default function Page() {
           <Link href="/mypage/newquiz" className={styles.createNewBtn}>
             + 새 질문 추가
           </Link>
+          {COLL_DATA.quiz.map((item: QUIZ, index: number) => {
+            return (
+              <div key={index} className={styles.quizWrapper}>
+                <span style={{ color: "blue" }}>ID: {item.id}</span>
+                <div>
+                  {COLL_DATA.access === "PRIVATE" ? "Private" : "Public"}
+                </div>
+                <p>{item.title}</p>
+                <span>{`${item.category.category} > ${item.category.collection}`}</span>
+                <p>{item.rate && `정답률 ${item.rate}%`}</p>
+                <span>{`Updated ${item.createdAt}`}</span>
+              </div>
+            );
+          })}
         </Container>
       </div>
     </>
