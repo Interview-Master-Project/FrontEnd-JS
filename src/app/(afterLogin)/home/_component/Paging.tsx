17f57092b812b2671d06083c322e503379a771a6
tim1007 <111577841+yuzunsang@users.noreply.github.com>
2024-10-07 14:58:44 +0900

    Feat: page인덱스 로직 완성

--- src/app/(afterLogin)/home/_component/Paging.tsx
+++ src/app/(afterLogin)/home/_component/Paging.tsx
@@ -1,35 +1,50 @@
 "use client";
 
-import IData, { IPageInfo } from "@/model/search-collections";
+import { IPageInfo } from "@/model/search-collections";
 import clsx from "clsx";
 import styles from "./paging.module.scss";
-import { useQuery } from "@apollo/client";
-import { SEARCH_COLLECTIONS } from "@/graphql/query";
 
 // 총 페이지, 현재 페이지 정보를 가져오고
 // click 시마다 다른 variables로 재요청
 
 interface Props {
   pageInfo: IPageInfo | undefined;
-  onChangeSkip: () => void;
+  onChangeSkip: (page: number) => void;
 }
 
 export default function Paging({ pageInfo, onChangeSkip }: Props) {
+  const { currentPage, hasNextPage, totalPages } = pageInfo as IPageInfo;
+
+  // totalPages 만큼 배열 크기와 1, 2, 3,... 생성
+  const pageArr = Array(totalPages)
+    .fill(1)
+    .map((value, idx) => value + idx);
+
   return (
     <div className={styles.pagingWrapper}>
-      <button>이전</button>
-      {/* {pageArr?.map((value, idx) => (
+      <button
+        disabled={currentPage === 1}
+        onClick={() => onChangeSkip(currentPage - 1)}
+      >
+        이전
+      </button>
+      {pageArr?.map((pageIdx, idx) => (
         <button
           key={idx}
-          onClick={() => handleClick(value)}
+          onClick={() => onChangeSkip(pageIdx)}
           className={clsx({
-            [styles.currentPage]: currentPage === value,
+            [styles.currentPage]: currentPage === pageIdx,
           })}
         >
-          {value}
+          {pageIdx}
         </button>
-      ))} */}
-      <button>다음</button>
+      ))}
+      <button
+        disabled={!hasNextPage}
+        onClick={() => onChangeSkip(currentPage + 1)}
+      >
+        다음
+      </button>
     </div>
   );
 }
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -2,13 +2,14 @@
 
 import Grid from "./_component/Grid";
 import Paging from "./_component/Paging";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
 import IData from "@/model/search-collections";
 import { SEARCH_COLLECTIONS } from "@/graphql/query";
 
 export default function Page() {
   const [skip, setSkip] = useState(0); // 컬렉션 슬라이싱
+
   const { data } = useQuery<IData>(SEARCH_COLLECTIONS, {
     variables: {
       keywords: [""],
@@ -17,7 +18,11 @@ export default function Page() {
     },
   });
 
-  const handleChangeSkip = () => {};
+  if (!data) return null;
+
+  const handleChangeSkip = (offset: number) => {
+    setSkip(6 * (offset - 1));
+  };
 
   return (
     <>
