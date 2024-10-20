c1d208b6d2901830d6230ef456c2f4e50cac39db
tim1007 <111577841+yuzunsang@users.noreply.github.com>
2024-10-20 20:49:51 +0900

    Feat: /explore Layout UI

--- src/app/(afterLogin)/explore/layout.tsx
+++ src/app/(afterLogin)/explore/layout.tsx
@@ -1,21 +1,5 @@
-// import SearchForm from "./_component/SearchForm";
-// import ResultHeader from "./_component/ResultHeader";
 import Search from "./_component/search/Search";
 import styles from "./layout.module.scss";
-
-// export default function Layout({ children }: React.PropsWithChildren) {
-//   return (
-//     <>
-//       <div className={styles.searchWall}>
-//         <SearchForm />
-//       </div>
-//       <div className={styles.resultHeader}>
-//         <ResultHeader />
-//       </div>
-//       {children}
-//     </>
-//   );
-// }
 
 export default function Layout({ children }: React.PropsWithChildren) {
   return (
