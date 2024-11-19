068ee93f18231438a28e919fff35997a20231aa2
tim1007 <111577841+yuzunsang@users.noreply.github.com>
2024-11-19 16:08:35 +0900

    Feat: 임시 프로필 이미지 Header 컴포넌트에서 수동 설정

--- src/app/(afterLogin)/_component/header/Profile.tsx
+++ src/app/(afterLogin)/_component/header/Profile.tsx
@@ -1,37 +1,43 @@
 "use client";
 
 import Link from "next/link";
-import { useState, useMemo, useEffect } from "react";
+import { useState, useMemo } from "react";
+import { useApolloClient } from "@apollo/client";
 import { createAvatar } from "@dicebear/core";
 import { thumbs } from "@dicebear/collection";
 import { useClientFetch } from "@/hooks/useClientFetch";
 import { ME, IData } from "@/graphql/query/me";
 import { Dropdown } from "@/app/_component/dropdown/Dropdown";
 import { useLogout } from "@/hooks/useLogout";
-import { useUserStore } from "@/store/useUserStore";
 import styles from "./profile.module.scss";
 
 export default function Profile() {
-  const { data, error, loading } = useClientFetch<IData>(ME, {}, true);
-  const [image, setImage] = useState<string | null>(null);
+  const { data, loading, error } = useClientFetch<IData>(ME, {}, true);
+
+  const client = useApolloClient();
   const [isOpen, setIsOpen] = useState(false);
   const { handleLogout } = useLogout();
 
-  useEffect(() => {
-    if (data?.me.imgUrl) setImage(data?.me.imgUrl);
-  }, [data?.me.imgUrl]);
+  if (loading) {
+    return <div>유저 정보 불러오는 중...</div>;
+  }
 
-  const { user: userInfo } = useUserStore();
-  const avatar = useMemo(() => {
-    return createAvatar(thumbs, {
-      seed: "4",
+  if (data && !data?.me.imgUrl) {
+    const { me }: IData = client.cache.readQuery({ query: ME })!;
+    const avatar = createAvatar(thumbs, {
+      seed: (+data?.me.id! / 5 + 1).toString(),
     }).toDataUriSync();
-  }, []);
 
-  const user = {
-    ...data,
-    image: image ?? avatar,
-  };
+    client.cache.writeQuery<IData>({
+      query: ME,
+      data: {
+        me: {
+          ...me,
+          imgUrl: avatar,
+        },
+      },
+    });
+  }
 
   const handleCloseDropdown = () => {
     setIsOpen(!isOpen);
@@ -41,10 +47,6 @@ export default function Profile() {
     setIsOpen(false);
   };
 
-  if (loading) {
-    return <div>유저 정보 불러오는 중...</div>;
-  }
-
   return (
     <Dropdown onClose={handleOffDropdown}>
       <Dropdown.Active
@@ -53,10 +55,10 @@ export default function Profile() {
         boxHeight={80}
       >
         <div className={styles.container}>
-          {user.image && (
+          {data?.me.imgUrl && (
             <img
-              src={user.image}
-              alt={`${user.me?.nickname}의 이미지`}
+              src={data?.me.imgUrl as string}
+              alt={`${data?.me.nickname}의 이미지`}
               width={36}
               height={36}
               className={styles.userImg}
